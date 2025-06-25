74a63055ad2339773554ebc8a133b7dddea73f43
Isak <16906089+kansson@users.noreply.github.com>
2025-06-25 21:01:30 +0200

    chore: publish snapshot versions

--- kmp-convex-core/build.gradle.kts
+++ kmp-convex-core/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.plugin.atomicfu)
@@ -32,5 +34,33 @@ android {
     compileSdk = 35
     defaultConfig {
         minSdk = 24
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    pom {
+        name = "kmp-convex"
+        description = "Convex for Kotlin Multiplatform"
+        inceptionYear = "2025"
+        url = "https://github.com/kansson/kmp-convex"
+        licenses {
+            licenses {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/mit"
+            }
+        }
+        developers {
+            developer {
+                id = "kansson"
+                name = "Isak Hansson"
+                url = "https://github.com/kansson"
+            }
+        }
+        scm {
+            url = "https://github.com/kansson/kmp-convex"
+            connection = "scm:git:https://github.com/kansson/kmp-convex.git"
+            developerConnection = "scm:git:ssh://git@github.com/kansson/kmp-convex.git"
+        }
     }
 }
